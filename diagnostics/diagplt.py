2ed515c41e502f2ef2dcdd73f740624a30e0f722
Nick Valverde <valverde667@gmail.com>
2020-06-09 11:35:44 -0400

    Create xz scatter plot function

--- diagnostics/diagplt.py
+++ diagnostics/diagplt.py
@@ -4,3 +4,29 @@ import pandas as pd
 import seaborn as sns
 
 import warp as wp
+
+
+
+
+
+def xzscatplot(beam,
+    scale = wp.mm,
+    markersize=.1,
+    color = 'k',
+    title = "x vs z",
+    titlesize = 16,
+    axlblsize = 14,
+    xlabel = "z[mm]",
+    ylabel = "x [mm]",
+    ):
+
+    x = beam.getx()/scale
+    z = beam.getx()/scale
+
+    fig, ax = plt.subplots()
+    ax.scatter(z,x, s =markersize, c=color)
+    ax.set_title(title, fontsize=titlesize)
+    ax.set_xlabel(xlabel, fontsize=axlblsize)
+    ax.set_ylabel(ylabel, fontsize=axlblsize)
+
+    return ax
